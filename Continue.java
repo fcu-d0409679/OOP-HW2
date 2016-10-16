6fd5ebaa6f704c44aed08c3ace495c3bc4e6c817
User-PC\User <jimhou28@gmail.com>
2016-10-16 15:42:13 +0800

    用for 迴圈做 continue執行，有附註解

---
+++ Continue.java
@@ -0,0 +1,17 @@
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		
+		for(int i=0;i<5;i++){
+			
+			if(i%2==0)continue;//continue執行時，從continue以下的程式都會無條件跳過，繼續做迴圈
+			
+			System.out.println(i);
+			
+		}
+				
+	}
+
+}
