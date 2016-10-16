22fba2864f3d64c093de8e64efd0d005efd9865f
User-PC\User <jimhou28@gmail.com>
2016-10-16 15:43:02 +0800

    用for 迴圈做 break執行，有附註解

---
+++ Break.java
@@ -0,0 +1,17 @@
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+
+		for(int i=0;i<5;i++){
+			
+			if(i>=2)break;//break執行時，程式都會無條件中斷，所以會跳出迴圈(本題i只到2就跳出了
+			
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
