ed311813e663e6e23760976b7644e291b285aec1
User-PC\User <jimhou28@gmail.com>
2016-10-22 15:15:54 +0800

    修改for迴圈條件

--- Break.java
+++ Break.java
@@ -6,11 +6,12 @@ public class Break {
 
 		for(int i=0;i<5;i++){
 			
-			if(i>=2)break;//break執行時，程式都會無條件中斷，所以會跳出迴圈(本題i只到2就跳出了
+			if(i==2)break;//break執行時，程式都會無條件中斷，所以會跳出迴圈(本題i只到2就跳出了
 			
 			System.out.println(i);
 			
 		}
+		
 		
 	}
 
